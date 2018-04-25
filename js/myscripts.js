c357de0141d796fb6ff0c4e2df55af77d1fc5905
Nabin Jaiswal <nabincodes@gmail.com>
2018-04-25 18:09:10 +0530

    changes in registration page, separate field for students and employee

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,7 +3,19 @@ $(document).ready(function(){
         e.preventDefault()
         $(this).tab('show')
       });
+
+    $('#student-form').css("display", "none");
+
 });
-    
+
+function showEmployee(){
+    document.getElementById('employee-form').style.display='block';
+    document.getElementById('student-form').style.display='none';
+}
+
+function showStudent(){
+    document.getElementById('student-form').style.display='block';
+    document.getElementById('employee-form').style.display='none';
+}
 
 
